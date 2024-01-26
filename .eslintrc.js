563576dc25a77e93b5db15bf02ea44eb5d72da3a
Manuel Pérez <manuel.perez@collaborator.audiense.com>
2024-01-26 17:00:52 +0000

    clean up project

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "env": {
+    "es2021": true
+  },
+  "extends": "eslint-config-standard",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+}
