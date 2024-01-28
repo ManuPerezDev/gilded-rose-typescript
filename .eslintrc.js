ec92815c2b3c858b13fc3edb4a5ad0405946fa16
Manuel Pérez <manuel.perez@collaborator.audiense.com>
2024-01-28 10:50:49 +0000

    dependenciy(eslint): install TS parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
-  "env": {
-    "es2021": true
+  env: {
+    es2021: true
   },
-  "extends": "eslint-config-standard",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+  extends: 'eslint-config-standard',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
 }
